511ff8f92498998a3a0f43b326a13c6ee6c6fc5d
mananatal <mananatal25@gmail.com>
2025-01-20 21:14:22 +0530

    Update ClerkProvider to specify afterSignOutUrl

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider >{children}</ClerkProvider>
+        <ClerkProvider afterSignOutUrl="/">{children}</ClerkProvider>
         <Toaster />
       </body>
     </html>
